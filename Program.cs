93121df6df3e1821b53b1804b730f2bb81b9a4b3
ChaveDireita <geometrianaoeuclidiana@gmail.com>
2021-02-25 19:56:29 -0300

    Uploaded files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace client
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MUWClient())
+                game.Run();
+        }
+    }
+}
